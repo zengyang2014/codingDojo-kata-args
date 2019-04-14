74fb650b418a9598c9874e46723b0392efae5e73
yzeng <yzeng@thoughtworks.com>
2019-04-14 15:54:08 +0800

    support to input multiple commands.

--- codingDojo.kata.args/src/main/java/Argument.java
+++ codingDojo.kata.args/src/main/java/Argument.java
@@ -1,24 +1,44 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.google.common.collect.Lists.newArrayList;
+
 class Argument {
-  public static final String LOGGING_COMMAND = "-l";
-  public static final String PORT_COMMAND = "-p";
+  public static final String LOGGING_COMMAND = "l";
+  public static final String PORT_COMMAND = "p";
   public static final int DEFAULT_PORT = 8080;
-  private String command;
+  public static final String MINUS = "-";
+  public static final String SPACE = " ";
+
+  private Map<String, String> arguments = new HashMap<>();
 
   public Argument(String command) {
-    this.command = command;
+    splitCommand(command);
   }
 
   public boolean parseLogging() {
-    if (this.command.equals(LOGGING_COMMAND)) {
+    if (arguments.containsKey(LOGGING_COMMAND)) {
       return true;
     }
     return false;
   }
 
   public int parsePort() {
-    if(this.command.split(" ")[0].equals(PORT_COMMAND))
-      return Integer.valueOf(this.command.split(" ")[1]);
+    if (arguments.containsKey(PORT_COMMAND))
+      return Integer.valueOf(arguments.get(PORT_COMMAND));
 
     return DEFAULT_PORT;
   }
+
+  private void splitCommand(String command) {
+    ArrayList<String> commands = newArrayList(command.split(MINUS));
+    commands.remove(0);
+
+    commands.forEach(cmd -> {
+      ArrayList<String> splitCommand = newArrayList(cmd.split(SPACE));
+      arguments.put(splitCommand.get(0), splitCommand.get(splitCommand.size() - 1));
+    });
+  }
+
 }
--- codingDojo.kata.args/src/test/java/ArgumentTest.java
+++ codingDojo.kata.args/src/test/java/ArgumentTest.java
@@ -18,20 +18,31 @@ public class ArgumentTest {
   }
 
   @Test
-  public void should_parse_port_command_and_value_when_there_is_port_command() {
+  public void should_parse_port_character_and_value_when_there_is_port_command() {
     Argument argument = new Argument("-p 10");
     Argument argumentOne = new Argument("-p 90");
 
-    assertEquals(argument.parsePort(), 10);
-    assertEquals(argumentOne.parsePort(), 90);
+    assertEquals(10, argument.parsePort());
+    assertEquals(90, argumentOne.parsePort());
   }
 
   @Test
-  public void should_parse_port_command_has_default_value_8080_when_there_is_no_port_command() {
+  public void should_parse_port_character_has_default_value_8080_when_there_is_no_port_command() {
     Argument argument = new Argument("");
     Argument argumentOne = new Argument("-l");
 
-    assertEquals(argument.parsePort(), 8080);
-    assertEquals(argumentOne.parsePort(), 8080);
+    assertEquals(8080, argument.parsePort());
+    assertEquals(8080, argumentOne.parsePort());
+  }
+
+  @Test
+  public void should_parse_logging_and_port_character_with_different_order() {
+    Argument argument = new Argument("-l -p 90");
+    Argument argumentOne = new Argument("-p 90 -l");
+
+    assertEquals(90, argument.parsePort());
+    assertTrue(argument.parseLogging());
+    assertEquals(90, argumentOne.parsePort());
+    assertTrue(argumentOne.parseLogging());
   }
 }
